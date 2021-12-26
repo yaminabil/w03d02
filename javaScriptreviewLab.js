a42a4be04b7822bdf587c7e7457181d74c4ad59f
bilal younes <bilal94.younes@gmail.com>
2021-12-26 15:24:43 -0500

    modified done

---
+++ javaScriptreviewLab.js
@@ -0,0 +1,32 @@
+
+// 1. How do we assign a value to a variable? A. With the assignment operator
+let a =3 ;
+
+//2. How do we change the value of a variable . by  assigning the new value to it ...
+
+a=4;//assigning the new value 
+
+//3.How do we assign an existing variable to a new variable 
+ 
+let newvariable =5 ;
+
+//4.Remind me, what are declare, assign, and define?
+//declare
+let x=1 ;
+const y=2;
+
+
+//assign  : give the varible a name like we did x and y 
+//define  : give the variables a value like 1 and 2 
+
+
+// 5.What is pseudocoding and why should you do it?
+// pseudocoding   is the logic behind the code and it doesn't have to be in any coding language 
+//it gives an idea how you solve the problem 
+
+// 6. what percentage of time should be spent about how you're going to solve a problem vs actually typing in code to solve it 
+
+
+
+
+
