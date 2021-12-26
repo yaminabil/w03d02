34ca3f7416333044bb89c8fead5561e34226804a
bilal younes <bilal94.younes@gmail.com>
2021-12-26 15:23:32 -0500

    new js file been added

--- classwork.js
+++ classwork.js
@@ -55,5 +55,61 @@ console .log ("hello world its so great");
 
     console.log (maxOfThree (1,2,3));
 
-
+    //class 
+     
+    class Cat {
+      constructor(name, age, cuteness){
+          this.name = name;
+          this.age = age;
+          this.cuteness = cuteness;
+          this.legs = 4;
+          this.hasClaws = true
+      }
+      scratch(){
+          if(this.age < 2 && this.hasClaws){
+              console.log('Your couch is destroyed')
+          } else {
+              console.log('nothing happens the cat cant scratch')
+          }
+      }
+  }
   
+  
+  const huey = new Cat('Huey P Kitten', 4, Infinity)
+  
+  console.log(huey)
+  huey.scratch()
+  
+  class SuperCat extends Cat {
+      constructor(name, age, cuteness, superPower){
+          super(name, age, cuteness)
+          this.superPower = superPower
+      }
+  }
+  
+  const patches = new SuperCat('Patches', 9, Infinity, 'flying')
+  console.log(patches)
+  
+  
+  class CrazyCatMan {
+      constructor(){
+          this.felineFriends = []
+      }
+      adoptCat(catName, age){
+          const newFriend = new Cat(catName,age, Infinity)
+          this.felineFriends.push(newFriend)
+      }
+  }
+  
+  const arthur = new CrazyCatMan()
+  
+  arthur.adoptCat('patches jr', 0, Infinity - 1)
+  
+  console.log(arthur)
+
+
+  //GIT
+  //initialize the git repo "git init "
+  //check my git status " git status "
+  //stage our commit "git add -A"
+  //
